7e236d5b89d84549d55d2e78f21727c076327b37
santhoshparamasivam <santhoshparamasiva78@gmail.com>
2024-09-12 20:59:59 +0530

    env variable tested

--- api/main.py
+++ api/main.py
@@ -1,15 +1,21 @@
 import os
+import json
+import base64
 import firebase_admin
 from firebase_admin import credentials, firestore
 from flask import Flask
 from flask_restful import Api, Resource
+
+service_account_base64 = os.getenv('GOOGLE_CREDENTIALS')
+service_account_json = base64.b64decode(service_account_base64).decode('utf-8')
+service_account_info = json.loads(service_account_json)
 
 app = Flask(__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {"data":"Hello World"}
+        return {"data":service_account_base64}
     
 @app.route('/favicon.ico')
 def favicon():
