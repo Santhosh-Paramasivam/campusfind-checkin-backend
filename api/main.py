5f5a1f2347dbf246b621dc637a662e6b66da5b5a
santhoshparamasivam <santhoshparamasiva78@gmail.com>
2024-09-12 21:05:57 +0530

    initialize firestore

--- api/main.py
+++ api/main.py
@@ -10,6 +10,12 @@ service_account_base64 = os.getenv('FIREBASE_AUTH_CREDENTIALS')
 service_account_json = base64.b64decode(service_account_base64).decode('utf-8')
 service_account_info = json.loads(service_account_json)
 
+cred = credentials.Certificate(service_account_info)
+firebase_admin.initialize_app(cred)
+
+# Initialize Firestore
+db = firestore.client()
+
 app = Flask(__name__)
 api = Api(app)
 
