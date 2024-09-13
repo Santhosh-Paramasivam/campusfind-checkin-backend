fb1e67ba1a2f8aacac3e46cfce579eb88152eadf
santhoshparamasivam <santhoshparamasiva78@gmail.com>
2024-09-13 17:16:03 +0530

    error fix 3

--- api/main.py
+++ api/main.py
@@ -111,7 +111,9 @@ class updateUserLocation(Resource):
         
         #print(data['mac_address'])
         #datatoreturn = getDocument('rfid_reader_location',('reader_mac_address','==',data['mac_address']),('location',))
-        data = getDocument('rfid_users',('rfid_uid','==',data['uid']),('user_id',))
+        uid = data.get('uid')
+        print(uid)
+        data = getDocument('rfid_users',('rfid_uid','==',uid),('user_id',))
         # updateDocument('rfid_users',('user_id','==',1),{"user_id":2})
         if not data:
            return {"Error":"User document not updated"}, 400
