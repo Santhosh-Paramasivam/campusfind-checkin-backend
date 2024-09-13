46666137c75268aa83e6bc7619fe91a534a05bf6
santhoshparamasivam <santhoshparamasiva78@gmail.com>
2024-09-13 13:55:17 +0530

    error corrected

--- api/main.py
+++ api/main.py
@@ -119,19 +119,19 @@ class updateUserLocation(Resource):
         if not apiKeyCheck(request):
             return {"Error":"Unauthorised access"},401
     
-        #data = request.json
+        data = request.json
         
-        #if not data:
-        #    return {"Error":"no input data sent"},400
-        #if not 'uid' in data:
-        #    return {"Error":"uid field not sent"},400
-        #if not 'mac_address' in data:
-        #    return {"Error":"mac_address field not sent"}    
+        if not data:
+            return {"Error":"no input data sent"},400
+        if not 'uid' in data:
+            return {"Error":"uid field not sent"},400
+        if not 'mac_address' in data:
+            return {"Error":"mac_address field not sent"},400    
         
         if not updateDocument('rfid_users',('user_id','==',1),{"user_id":2}):
            return {"Error":"User document not updated"}, 400
         else:
-            return {"Success":"User document not updated"}, 200
+            return {"Success":"User document updated"}, 200
         
             
 class sendScannedUID(Resource):
