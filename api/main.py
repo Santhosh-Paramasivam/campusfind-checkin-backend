0a7184f42a64ba26f3e1562cc5fb39740403a261
santhoshparamasivam <santhoshparamasiva78@gmail.com>
2024-09-13 17:02:49 +0530

    clean up

--- api/main.py
+++ api/main.py
@@ -116,7 +116,7 @@ class updateUserLocation(Resource):
         if not data:
            return {"Error":"User document not updated"}, 400
         else:
-            return {"Success":data['user_id']}, 200
+            return {"Success":"User document updated","user_id":data['user_id']}, 200
         
             
 class sendScannedUID(Resource):
