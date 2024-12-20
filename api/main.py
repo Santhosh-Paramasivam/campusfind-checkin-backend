367e9e260a3f4be80d6cf2b33603d73588b9cb60
santhoshparamasivam <santhoshparamasivam78@gmail.com>
2024-12-20 12:00:04 +0530

    completed api key check trial

--- api/main.py
+++ api/main.py
@@ -28,8 +28,6 @@ def uniqueAPIKeyCheck(institution_id, unique_api_key):
     if not api_key or api_key['api_key'] != unique_api_key:
         return False
     else:
-        data = getDocumentSecure(institution_id,'institution_members',('rfid_uid','==',"F3:A7:16:30"),('rfid_location','id','in_room'))
-        print(data)
         return True
 
 
@@ -44,7 +42,27 @@ def updateDocument(collection_id, query, updated_values):
     collection_ref = db.collection(collection_id)
 
     query_ref = collection_ref.where(query[0], query[1], query[2])
-    collection_ref.where()
+
+    if not query_ref:
+        return False
+    
+    docs = query_ref.stream()
+    doc_updated = False
+
+    for doc in docs:
+        doc_ref = db.collection(collection_id).document(doc.id)
+
+        doc_ref.update(updated_values)
+        doc_updated = True
+
+    return doc_updated
+
+def updateDocumentSecure(institution_id, collection_id, query, updated_values):
+
+    collection_ref = db.collection(collection_id)
+
+    query_ref = collection_ref.where(filter=FieldFilter('institution_id','==',institution_id)).where(filter=FieldFilter(query[0], query[1], query[2]))
+    
     if not query_ref:
         return False
     
@@ -97,11 +115,55 @@ def getDocument(collection_id, query, values_to_get):
 
 class UpdateUserLocationSecure(Resource):
     def post(self):
+        if not apiKeyCheck(request):
+            return {"error":"Unauthorised access, incorrect public api key"},401
+        
         data = request.json
-        if(uniqueAPIKeyCheck(data['institution_id'], data['api_key'])):
-            return {"success":"Authorised!"},200
+
+        if not data:
+            return {"error":"No input data sent"},400
+        if 'uid' not in data:
+            return {'error':"uid field not sent"},400
+        if 'mac_address' not in data:
+            return {"error":"mac_address field not sent"},400
+        if 'entry_time' not in data:
+            return {"error":"entry_time field not sent"},400
+        if 'api_key' not in data:
+            return {"error":"api_key field not sent"},400
+        if 'institution_id' not in data:
+            return {"error":"institution_id field not sent"},400
+            
+        mac_address = data['mac_address']
+        uid = data['uid']
+        institution_id = data['institution_id']
+        private_api_key = data['api_key']
+        date_time = datetime.fromisoformat(data['entry_time'].replace("Z", "+00:00"))
+
+        if not uniqueAPIKeyCheck(institution_id,private_api_key):
+            return {"error":"Unauthorised institution access, incorrect private api key"},400
+
+        rfid_users = getDocumentSecure(institution_id,'institution_members',('rfid_uid','==',uid),('rfid_location','id','in_room'))
+        if not rfid_users:
+            return {"error":"Invalid UID"},400
+        previous_location = rfid_users['rfid_location']
+        user_id = rfid_users['id']
+        in_room = rfid_users['in_room']
+
+        reader_mac_address = getDocumentSecure(institution_id,"rfid_reader_location",("reader_mac_address","==",mac_address),("location",))
+        if not reader_mac_address:
+            return {"error":"Invalid MAC address"},400
+        current_location = reader_mac_address['location']
+
+        if(current_location != previous_location):
+            in_room = True
         else:
-            return {"error":"Unauthorised"},401
+            in_room = not in_room
+
+        docUpdated = updateDocumentSecure('institution_members',('id','==',user_id),{"rfid_location":current_location, "in_room":in_room, "last_location_entry":date_time})
+        if not docUpdated:
+            return {"error":"Unexpected error occured"},500
+
+        return {"success":"User document updated"},200
 
 class UpdateUserLocation(Resource):
     def post(self):
